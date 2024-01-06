745799ed375ad90927fc95e01e0248b6bafa460d
Hannahphoto <hannahprice67@gmail.com>
2024-01-05 21:52:24 -0500

    beginnings

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/');
+
+const sequelize = require('.config/connection');
+const SequelizeBlog = require('connect-session-sequelize')(session.Blog);
+
+const app = express();
+const PORT = process.env.PORT || 9001;
+
+// set up Handlebars with Helpers
+const hbs = exphbs.creat({helpers});
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+        maxAge: 300000, 
+        httpOnly: true, 
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false, 
+    saveUninitialized: true, 
+    blog: new SequelizeBlog({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+// inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({force: false}).then(()=>{
+    app.listen(PORT, ()=> console.log('Now listening to http://localhost:${PORT}'));
+});
