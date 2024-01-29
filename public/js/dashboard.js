ff1583ed02a19a60bb1dab09e2626caece29789b
Hannahphoto <hannahprice67@gmail.com>
2024-01-28 19:21:51 -0500

    working on dashboard page with html

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,22 +1,15 @@
 // THEN I am taken to the dashboard and presented with any blog posts I have already created and the option to add a new blog post
-// WHEN I click on the button to add a new blog post
-// THEN I am prompted to enter both a title and contents for my blog post
-// WHEN I click on the button to create a new blog post
-// THEN the title and contents of my post are saved and I am taken back to an updated dashboard with my new blog post
-// WHEN I click on one of my existing posts in the dashboard
-// THEN I am able to delete or update my post and taken back to an updated dashboard
 
-
-const newFromHandler = async (event) => {
+const createPostHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#project-name');
-    const description = document.querySelector('#project-desc');
+    const title = document.querySelector('#blog-title');
+    const content = document.querySelector('#blog-content');
 
-    if(name && description){
+    if(title && content){
         const response = await fetch(`/api/dashboard`, {
             method: 'POST',
-            body: JSON.stringify({name, description}),
+            body: JSON.stringify({title, content}),
             headers:{
                 'Content-Type': 'application/json',
             },
@@ -44,6 +37,24 @@ const delButtonHandler = async(event) => {
     }}
 };
 
-document.querySelector('.new-project-form').addEventListener('submit', newFromHandler);
+// WHEN I click on the button to add a new blog post
+// THEN I am prompted to enter both a title and contents for my blog post
+document.querySelector('#btn-newPost ').addEventListener('submit', function(){
+    document.getElementById('postBlog').style.display="block";
+});
 
-document.querySelector('.project-list').addEventListener('click', delButtonHandler);
+// WHEN I click on the button to create a new blog post
+// THEN the title and contents of my post are saved and I am taken back to an updated dashboard with my new blog post
+document.querySelectord('btn-createPost').addEventListener('submit', function(){
+    //add logic to handle fomr submission
+
+    //hide form after submit.
+    document.getElementById('postBlog').style.display="none";
+})
+
+// WHEN I click on one of my existing posts in the dashboard
+// THEN I am able to delete or update my post and taken back to an updated dashboard
+document.querySelector('.blog-list').addEventListener('click', delButtonHandler());
+
+
+
