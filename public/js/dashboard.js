e236d0361036f9db740dd45d11e264491c5171c1
Hannahphoto <hannahprice67@gmail.com>
2024-02-25 23:08:57 -0500

    new post button working, need to get form to work

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,16 +1,32 @@
-// const newPostButton = document.getElementById("#btn-newPost");
-// const createPostButton = document.getElementById('#btn-createPost');
 
+document.addEventListener('DOMContentLoaded',function(){
+    //select the new post button
+    const newPostButton = document.getElementById('btn-newPost');
+    const containerPosts = document.querySelectorAll('.containerPost');
+    const postBlogForm = document.getElementById('postBlog');
+
+    console.log("New Post Button:", newPostButton);
+
+    //add click event listenter to the button
+    newPostButton.addEventListener('click', function(){
+        //toggle the visibility of the form
+       containerPosts.forEach(function(containerPost){
+        containerPost.style.display = 'none';
+       });
+
+       postBlogForm.style.display = 'block';
+    });
+});
 // THEN I am taken to the dashboard and presented with any blog posts I have already created and the option to add a new blog post
 
 const createPostHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#blog-title');
-    const description = document.querySelector('#blog-content');
+    const name = document.querySelector('#title');
+    const description = document.querySelector('#content');
 
     if(name && description){
-        const response = await fetch(`/dashboard`, {
+        const response = await fetch(`/api/dashboard`, {
             method: 'POST',
             body: JSON.stringify({name, description}),
             headers:{
@@ -26,10 +42,12 @@ const createPostHandler = async (event) => {
     }
 };
 
+// document.querySelector("#btn-newPost").addEventListener('submit', createPostHandler);
+
 const delButtonHandler = async(event) => {
     if(event.target.hasAttribute('data-id')){
 
-    const response = await fetch(`/dashboard/${id}`,{
+    const response = await fetch(`/api/dashboard/${id}`,{
         method: 'DELETE',
     });
 
@@ -50,24 +68,34 @@ const hideCreateBlogForm = () => {
 
 // WHEN I click on the button to add a new blog post
 // THEN I am prompted to enter both a title and contents for my blog post
-newPostButton.addEventListener('click', function(){
-    showCreateBlogForm();
-});
+// newPostButton.addEventListener('click', function(){
+//     showCreateBlogForm();
+// });
 
 // WHEN I click on the button to create a new blog post
 // THEN the title and contents of my post are saved and I am taken back to an updated dashboard with my new blog post
-createPostButton.addEventListener('click', function(){
-    //add logic to handle fomr submission
-    createPostHandler();
-    //update dashboard with list
+// createPostButton.addEventListener('click', function(){
+//     //add logic to handle fomr submission
+//     createPostHandler();
+//     //update dashboard with list
 
-    //hide form after submit.
-    hideCreateBlogForm();
-});
+//     //hide form after submit.
+//     hideCreateBlogForm();
+// });
 
 // WHEN I click on one of my existing posts in the dashboard
 // THEN I am able to delete or update my post and taken back to an updated dashboard
-document.querySelector('.blog-list').addEventListener('click', delButtonHandler());
 
 
 
+
+// document.querySelector('.blog-list').addEventListener('click', delButtonHandler());
+
+
+
+// if(postBlogForm){
+//     postBlogForm.style.display = (postBlogForm.style.display === 'none' || postBlogForm.style.display === '' ) ? 'block': 'none';
+//     }else{
+//         console.error("post blog form not found");
+//     }
+// });
